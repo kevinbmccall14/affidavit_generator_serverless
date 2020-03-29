480bb695f850590010783a977c6c6ac89351baed
Kevin McCall <kevinbmccall14@gmail.com>
2020-03-29 09:59:23 -0400

    save case item in DynamoDB after processing

--- functions/getAnalysis.py
+++ functions/getAnalysis.py
@@ -1,48 +1,89 @@
 import json
+import uuid
 import boto3
 from helpers.textractParser import Document
+from decimal import Decimal
 
 textract = boto3.client('textract')
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('CasesTable')
+
+
+def removeEmptyString(dic):
+    for e in dic:
+        if isinstance(dic[e], dict):
+            dic[e] = removeEmptyString(dic[e])
+        if (isinstance(dic[e], str) and dic[e] == ""):
+            dic[e] = None
+        if isinstance(dic[e], list):
+            for entry in dic[e]:
+                removeEmptyString(entry)
+    return dic
 
 
 def processDocument(doc):
+    # item to PUT in DynamoDB
+    item = {
+        'id': str(uuid.uuid4()),
+        'pages': []
+    }
     for page in doc.pages:
-        print("PAGE\n====================")
+        newPage = {
+            'lines': [],
+            'tables': [],
+            'fields': [],
+        }
         for line in page.lines:
-            print("Line: {}--{}".format(line.text, line.confidence))
+            newLine = {
+                'text': line.text,
+                'confidence': Decimal(line.confidence),
+                'words': [],
+            }
             for word in line.words:
-                print("Word: {}--{}".format(word.text, word.confidence))
+                newLine['words'].append({
+                    'text': word.text,
+                    'confidence': Decimal(word.confidence),
+                })
+            newPage['lines'].append(newLine)
         for table in page.tables:
-            print("TABLE\n====================")
+            newTable = {'cells': []}
             for r, row in enumerate(table.rows):
                 for c, cell in enumerate(row.cells):
-                    print("Table[{}][{}] = {}-{}".format(r,
-                                                         c, cell.text, cell.confidence))
-        print("Form (key/values)\n====================")
+                    newTable['cells'].append({
+                        'row': r,
+                        'column': c,
+                        'text': cell.text,
+                        'confidence': Decimal(cell.confidence)
+                    })
+            newPage['tables'].append(newTable)
         for field in page.form.fields:
+            newField = {}
             k = ""
             v = ""
             if(field.key):
                 k = field.key.text
             if(field.value):
                 v = field.value.text
-            print("Field: Key: {}, Value: {}".format(k, v))
+            newPage['fields'].append({
+                'key': k,
+                'value': v
+            })
+        item['pages'].append(newPage)
+    return removeEmptyString(item)
 
-        # Search field by key
-        key = "CLAIM NUMBER"
-        print("\nSearch field by key ({}):\n====================".format(key))
-        fields = page.form.searchFieldsByKey(key)
-        for field in fields:
-            print("Field: Key: {}, Value: {}".format(field.key, field.value))
+    # Example Searching for field could be used to find values
+    # key = "CLAIM NUMBER"
+    # print("\nSearch field by key ({}):\n====================".format(key))
+    # fields = page.form.searchFieldsByKey(key)
+    # for field in fields:
+    #     print("Field: Key: {}, Value: {}".format(field.key, field.value))
 
 
 def handler(event, context):
     # Parse JobID from TextractQueue
-    print(event)
     for record in event['Records']:
         message = json.loads(record["body"])["Message"]
         parsed_message = json.loads(message)
-    print(parsed_message)
 
     # 500 level error will keep event on TextractQueue
     # TextractQueue can configure max retry attempts and event age
@@ -52,13 +93,19 @@ def handler(event, context):
             'body': json.dumps(parsed_message)
         }
 
-    # Get Successfull Document Text JSON
-    response = textract.get_document_analysis(
+    print('Found successful job:', parsed_message["JobId"])
+
+    # Get successfull analysis JSON
+    analysis = textract.get_document_analysis(
         JobId=parsed_message["JobId"]
     )
 
-    doc = Document(response)
-    processDocument(doc)
+    # parse json into Document object with textractParser
+    doc = Document(analysis)
+    # process parsed document into dictionary item
+    item = processDocument(doc)
+    # save item into DynamoDB
+    response = table.put_item(Item=item)
 
     return {
         'statusCode': 200,
